75227a46d873e47245c09e9682306a66e212ddce
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 17:14:45 -0300

    Update cravefood.sh

--- partials/cravefood.sh
+++ partials/cravefood.sh
@@ -16,6 +16,12 @@ cd ~/
 # ----------------------------------------
 git clone git@github.com:CraveFood/webapp.git
 cd webapp
-npm install
+# node-sass requires node-gyp that requires gyp that uses Python 2
+# there is a 2009's open issue for supporting Python 3
+# at https://bugs.chromium.org/p/gyp/issues/detail?id=36
+# meanwhile, it is unfortunate but we have to keep the
+# Python 2 around, so....
+sudo apt install python2.7 
+npm install --python=python2.7
 cd ~/
 
