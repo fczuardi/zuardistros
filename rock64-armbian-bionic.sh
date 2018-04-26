2fb18faaad8a1eb49d7092f9dbc3b156e1632af7
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 09:51:42 -0300

    base packages for ubuntu bionic remote desktop

---
+++ rock64-armbian-bionic.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+PACKAGES=()
+
+
+# mdns / hostname.local discoverability
+# ----------------------------------------
+PACKAGES+=(
+  avahi-daemon
+)
+
+
+# remote desktop
+# ----------------------------------------
+PACKAGES+=(
+  tigervnc-standalone-server
+)
+# suggested: xfonts-100dpi xfonts-scalable
+# recommended: xfonts-base x11-xserver-utils
+
+
+# window manager
+# ----------------------------------------
+PACKAGES+=(
+  i3
+  i3status
+  i3lock
+  suckless-tools
+)
+
+# suggested: mesa-utils pulseaudio stterm surf
+# recommended: dunst xfonts-base libanyevent-i3-perl libjson-xs-perl rxvt-unicode
+
+
+# Install.
+apt -y update
+apt -y install ${PACKAGES[@]}
