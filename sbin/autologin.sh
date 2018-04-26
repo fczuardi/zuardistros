2d075213ca5da346f0a484ebccdcf0cc5bf2dd1e
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 13:16:03 -0300

    autologin on debian and ubuntu

---
+++ sbin/autologin.sh
@@ -0,0 +1,5 @@
+# autologin
+# ----------------------------------------
+cp /lib/systemd/system/getty@.service /lib/systemd/system/getty@.service.`date --iso`.bk
+read -p "Username for autologin: " username
+sed -i -e "s/agetty -.*/agetty --autologin ${username} %I \$TERM/" /lib/systemd/system/getty@.service
