2f5194d14cc57834a462e808130433271773ea9d
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 14:18:20 -0300

    Update bashrc_vnc.sh

--- partials/bashrc_vnc.sh
+++ partials/bashrc_vnc.sh
@@ -1,2 +1,5 @@
+# setup vnc server auto-launch
+# ------------------------------
 read -p "Extra arguments for the vnc server (ex -geometry 2560x1080): " vnc_extra_args
 tee <<< "tigervncserver -useold -localhost no ${vnc_extra_args}" >> ~/.bashrc
+
