e9ac860b708815ffeaeaad784902d9294bbdec73
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 16:15:35 -0300

    Update cravefood.sh

--- partials/cravefood.sh
+++ partials/cravefood.sh
@@ -1,4 +1,11 @@
 # CraveFood Open Source Projects
 # ----------------------------------------
+# requires packages_python
+# requires nodeenv
+nodeenv node10
+source ./node10/bin/activate
+npm i -g yarn
 git clone git@github.com:CraveFood/farmblocks.git
+cd farmblocks
+yarn install
 
