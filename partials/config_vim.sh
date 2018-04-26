4fa9b52c9144b8be4e73145d7911e0764e77aeb9
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 15:16:02 -0300

    Create config_vim.sh

---
+++ partials/config_vim.sh
@@ -0,0 +1,5 @@
+# vim setup
+# ----------------------------------------
+mkdir -p ~/.vim/undo ~/.vim/tmp
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/fczuardi/dotfiles/master/.vimrc
