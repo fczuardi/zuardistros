b8c9afa29e14a49bc331bca53792401c28b737be
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-26 13:33:36 -0300

    change backup date format

--- sbin/autologin.sh
+++ sbin/autologin.sh
@@ -1,5 +1,5 @@
 # autologin
 # ----------------------------------------
-cp /lib/systemd/system/getty@.service /lib/systemd/system/getty@.service.`date --iso`.bk
+cp /lib/systemd/system/getty@.service /lib/systemd/system/getty@.service.`date +%s`.bk
 read -p "Username for autologin: " username
 sed -i -e "s/agetty -.*/agetty --autologin ${username} %I \$TERM/" /lib/systemd/system/getty@.service
