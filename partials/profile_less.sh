58091626514647781c98f482b5b772e30c074c28
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 15:02:19 -0300

    Create profile_less.sh

---
+++ partials/profile_less.sh
@@ -0,0 +1,8 @@
+# Less default settings
+# ----------------------------------------
+# F: Quit if entire file fits on first screen.
+# R: Output "raw" control characters.
+# X: Don't use termcap init/deinit strings.
+# W: Highlight first new line after any forward movement.
+export LESS=FRXW
+
