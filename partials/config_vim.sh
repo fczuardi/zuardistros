0dead1957626706f656f0bf4144400f0affab23e
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 14:57:23 -0300

    Update config_vim.sh

--- partials/config_vim.sh
+++ partials/config_vim.sh
@@ -3,3 +3,4 @@
 mkdir -p ~/.vim/undo ~/.vim/tmp
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/fczuardi/dotfiles/master/.vimrc
+
