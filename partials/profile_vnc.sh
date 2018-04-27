5139ebdfd77fd702b4255217d6b7796a853f3a05
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 10:32:49 -0300

    remove config prompt and assume the var will be there

--- partials/profile_vnc.sh
+++ partials/profile_vnc.sh
@@ -1,6 +1,5 @@
 # setup vnc server auto-launch
 # ----------------------------------------
-read -p "Extra arguments for the vnc server (ex -localhost no -geometry 2560x1080): " vnc_extra_args
-tee <<< "tigervncserver -useold ${vnc_extra_args}" >> ~/.profile
-tee <<< "echo Remote desktop running, use ${bold}ssh $(whoami)@$(hostname) -L 5901:localhost:5901${normal} to connect or ${bold}vncserver -kill${normal} to stop it" >> ~/.profile
+tigervncserver -useold ${vnc_extra_args}
+echo Remote desktop running, use ${bold}ssh $(whoami)@$(hostname) -L 5901:localhost:5901${normal} to connect or ${bold}vncserver -kill${normal} to stop it
 
