3c94d15b48f2c77d85638c73870f51e6040bc394
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 18:31:33 -0300

    Create alacritty.sh

---
+++ partials/alacritty.sh
@@ -0,0 +1,7 @@
+# Alacritty Terminal
+# ----------------------------------------
+# requires: rust/rustup
+rustup override set stable
+rustup update stable
+cargo install --git https://github.com/jwilm/alacritty
+
