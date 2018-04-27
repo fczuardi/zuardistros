9cc2e31280ccedc3e068821fb0868e481fd25ffe
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 10:41:17 -0300

    only set the basics
    if users want they can edit .profile themselves

--- partials/profile_vnc.sh
+++ partials/profile_vnc.sh
@@ -1,5 +1,5 @@
 # setup vnc server auto-launch
 # ----------------------------------------
-tigervncserver -useold ${vnc_extra_args}
+tigervncserver -useold
 echo Remote desktop running, use ${bold}ssh $(whoami)@$(hostname) -L 5901:localhost:5901${normal} to connect or ${bold}vncserver -kill${normal} to stop it
 
