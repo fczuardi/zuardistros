801ee581c37f673cb706adb35a0958a6540c26c3
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 10:24:23 -0300

    make it available only to localhost
    and teach how to forward ports with ssh

--- partials/profile_vnc.sh
+++ partials/profile_vnc.sh
@@ -1,5 +1,6 @@
 # setup vnc server auto-launch
 # ------------------------------
-read -p "Extra arguments for the vnc server (ex -geometry 2560x1080): " vnc_extra_args
-tee <<< "tigervncserver -useold -localhost no ${vnc_extra_args}" >> ~/.profile
+read -p "Extra arguments for the vnc server (ex -localhost no -geometry 2560x1080): " vnc_extra_args
+tee <<< "tigervncserver -useold ${vnc_extra_args}" >> ~/.profile
+tee <<< "echo Remote desktop running, use ${bold}ssh $(whoami)@$(hostname) -L 5901:localhost:5901${normal} to connect or ${bold}vncserver -kill${normal} to stop it" >> ~/.profile
 
