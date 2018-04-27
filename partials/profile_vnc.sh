0cd383995afb8dc5dbec03dc6f594803ff6282bc
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-27 10:42:03 -0300

    Update profile_vnc.sh

--- partials/profile_vnc.sh
+++ partials/profile_vnc.sh
@@ -1,5 +1,5 @@
 # setup vnc server auto-launch
 # ----------------------------------------
 tigervncserver -useold
-echo Remote desktop running, use ${bold}ssh $(whoami)@$(hostname) -L 5901:localhost:5901${normal} to connect or ${bold}vncserver -kill${normal} to stop it
+echo Remote desktop running, use ${bold}ssh $(whoami)@$(hostname).local -L 5901:localhost:5901${normal} to connect or ${bold}vncserver -kill${normal} to stop it
 
