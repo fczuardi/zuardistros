897d6164acab3389e8fa21a4fb1628a29edb472b
Fabricio C Zuardi <fabricio@fabricio.org>
2018-05-01 10:09:18 -0300

    packages for hardware accelerated opengl es

--- partials/packages_opengl.sh
+++ partials/packages_opengl.sh
@@ -1,9 +1,13 @@
 # Hardware acceleration?
 # ----------------------------------------
 PACKAGES+=(
-  libmali-rk-utgard-450-r7p0-gbm
+  libmali-rk-utgard-450-r7p0
   libdrm-rockchip1
   gstreamer1.0-rockchip1
+  mesa-utils
+  mesa-utils-extra
+  glmark2
+  glmark2-es2
 )
 
 
