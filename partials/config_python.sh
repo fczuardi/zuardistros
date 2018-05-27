ebd4db691ae51e2a083a559ec0532ae79c653871
Fabricio C Zuardi <fabricio@fabricio.org>
2018-05-27 03:45:13 +0000

    include python2.7 packages and link creation

--- partials/config_python.sh
+++ partials/config_python.sh
@@ -1,3 +1,4 @@
 # python setup
 # ----------------------------------------
 sudo ln -s `which python3` /usr/bin/python 
+sudo ln -s `which python2.7` /usr/bin/python2 
--- partials/packages_python.sh
+++ partials/packages_python.sh
@@ -6,6 +6,7 @@ PACKAGES+=(
   python3-setuptools
   python3-virtualenv
   python3-venv
+  python2.7-minimal
   libssl-dev
 )
 
